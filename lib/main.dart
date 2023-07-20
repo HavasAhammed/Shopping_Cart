aad867291f1a765a2459833d1fab85253672120d
Hifa <hifanaval214@gmail.com>
2023-07-20 14:09:08 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_cart/provider/cart_provider.dart';
+import 'package:grocery_cart/screens/product_list.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(providers: [
+      ChangeNotifierProvider.value(value: CartProvider(),
+      )
+    ],
+    // ChangeNotifierProvider(
+    //   create: (_) => CartProvider(),
+      child: 
+      // Builder(builder: (BuildContext context){
+      //   return 
+        MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home:  const ProductListScreen(),
+    )
+      );
+    
+    
+  }
+}
+ 
